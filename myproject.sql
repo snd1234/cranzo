8da27334e9ec13666008c8ec613fa7a7bd60521a
@snd1234 <sandeepkp23@gmail.com>
2026-01-04 16:54:28 +0530

    Changes

--- myproject.sql
+++ myproject.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 06, 2025 at 03:56 PM
+-- Generation Time: Jan 04, 2026 at 12:20 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -126,6 +126,29 @@ CREATE TABLE `job_batches` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `main_category`
+--
+
+CREATE TABLE `main_category` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 1 COMMENT '0-Inactive,1-Active',
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `main_category`
+--
+
+INSERT INTO `main_category` (`id`, `name`, `status`, `created_at`) VALUES
+(1, 'Instruments', 1, '2026-01-04 11:50:26'),
+(2, 'Consumables', 1, '2026-01-04 11:50:26'),
+(3, 'Informatics', 1, '2026-01-04 11:51:15'),
+(4, 'Our Services', 1, '2026-01-04 11:51:15');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `migrations`
 --
 
@@ -148,6 +171,61 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `pages`
+--
+
+CREATE TABLE `pages` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `page_category_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `body` longtext DEFAULT NULL,
+  `status` enum('draft','published','archived') DEFAULT 'draft',
+  `meta_title` varchar(256) DEFAULT NULL,
+  `meta_description` text DEFAULT NULL,
+  `meta_keywords` text DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `published_at` datetime DEFAULT NULL,
+  `created_by` bigint(20) UNSIGNED DEFAULT NULL,
+  `updated_by` bigint(20) UNSIGNED DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pages`
+--
+
+INSERT INTO `pages` (`id`, `page_category_id`, `title`, `slug`, `body`, `status`, `meta_title`, `meta_description`, `meta_keywords`, `description`, `published_at`, `created_by`, `updated_by`, `created_at`, `updated_at`) VALUES
+(1, NULL, 'sddsd', 'asdadsadsda', '<p>adddad</p>', 'draft', 'sdsd', 'sdds', 'sdds', 'ddadad', NULL, 1, 1, '2025-11-29 04:17:38', '2025-11-29 04:17:38'),
+(2, NULL, 'Test 1111', 'product-category', NULL, 'draft', NULL, NULL, NULL, 'SDadaddad', NULL, 1, 1, '2026-01-04 02:34:05', '2026-01-04 02:34:05');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `page_category`
+--
+
+CREATE TABLE `page_category` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `page_category`
+--
+
+INSERT INTO `page_category` (`id`, `name`, `slug`, `description`, `created_at`, `updated_at`) VALUES
+(1, 'About Us', 'about-us', 'Abous Us page', NULL, NULL),
+(2, 'Contact Us', 'contact-us', 'Contact Us page', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `password_reset_tokens`
 --
 
@@ -156,6 +234,90 @@ CREATE TABLE `password_reset_tokens` (
   `token` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `category_id` bigint(20) UNSIGNED NOT NULL,
+  `sub_category_id` bigint(20) UNSIGNED NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `short_description` text DEFAULT NULL,
+  `description` longtext NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `status` tinyint(2) DEFAULT 1 COMMENT '0-Inactive,1-Active',
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_by` bigint(20) UNSIGNED NOT NULL,
+  `updated_by` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `category_id`, `sub_category_id`, `title`, `slug`, `short_description`, `description`, `image`, `status`, `created_at`, `updated_at`, `created_by`, `updated_by`) VALUES
+(1, 1, 1, 'KingFisher Duo Prime', 'KingFisher-Duo-Prime', NULL, '<p><strong>KingFisher Duo Prime</strong></p><p>The Thermo Scientific KingFisher Duo Prime is the most versatile compact benchtop sample preparation instrument in the lab for 6 or 12 samples per run. It offers consistent extraction and purification of DNA, RNA, proteins, and cells, and includes an ultraviolet lamp for decontamination. The KingFisher Duo Prime is an affordable choice for academic or small labs.</p><h4>Features</h4><p>In addition to the common features of all KingFisher Purification Systems, the Duo Prime offers:</p><h4>Automated purification</h4><p>6–12 samples per run</p><h4>Choose from two plate formats</h4><p>Wide 20 μL–5 mL volume range</p><h4>Download protocols from our library</h4><p>Or easily customize protocols using ThermoScientific BindIt Software</p><h4>Easy to install and run</h4><p>Ready to run in 10 minutes or less</p><h4>Simple workflow</h4><p>Just follow the on-screen instructions</p><h4>Ultraviolet lamp</h4><p>Decontamination is standard</p><h4>Applications</h4><p>The KingFisher Duo Prime can gently and efficiently isolate and purify nucleic acids, proteins, and cells from a range of sample types for a wide variety of downstream applications. For a selection of application kits, reagents, and protocols that run on the KingFisher Duo Prime, reach out to us.</p><h4>Protocols and software</h4><p>Thermo Scientific BindIt software is included with KingFisher Duo Prime and Flex instruments. The software makes it easy to create, download, run, modify, and store protocols for your KingFisher applications.</p><p>Protocols are available for nucleic acid and protein purification procedures with reagent kits. The software allows you to define and edit steps, parameters, plates, and reagents for every part of your protocol—bind, wash, and elute.</p>', NULL, 1, '2026-01-04 05:04:25', '2026-01-04 05:04:25', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_category`
+--
+
+CREATE TABLE `product_category` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `main_category_id` bigint(20) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `status` tinyint(1) DEFAULT 1 COMMENT '1- Active, 0 - Inactive',
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_by` bigint(20) UNSIGNED NOT NULL,
+  `updated_by` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product_category`
+--
+
+INSERT INTO `product_category` (`id`, `main_category_id`, `name`, `slug`, `description`, `status`, `created_at`, `updated_at`, `created_by`, `updated_by`) VALUES
+(2, 1, 'GBA Analyzer', 'GBA-Analyzer', 'GBA Analyzer', 1, '2026-01-04 05:38:35', '2026-01-04 05:38:35', 1, 1),
+(3, 1, 'GBA Solutions', 'GBA-Solutions', 'GBA Solutions', 1, '2026-01-04 05:39:01', '2026-01-04 05:39:01', 1, 1),
+(4, 2, 'LC Columns', 'LC-Columns', 'LC Columns', 1, '2026-01-04 05:39:38', '2026-01-04 05:39:38', 1, 1),
+(5, 3, 'Chromatography Data', 'Chromatography-Data', 'addsd', 1, '2026-01-04 05:40:29', '2026-01-04 05:40:29', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_sub_category`
+--
+
+CREATE TABLE `product_sub_category` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `category_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `status` tinyint(1) DEFAULT 1,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_by` bigint(20) UNSIGNED NOT NULL,
+  `updated_by` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product_sub_category`
+--
+
+INSERT INTO `product_sub_category` (`id`, `category_id`, `name`, `slug`, `description`, `status`, `created_at`, `updated_at`, `created_by`, `updated_by`) VALUES
+(1, 1, 'ssddddssd dff', 'sddddsddddsd', 'sddsds fff', 1, '2026-01-04 04:00:24', '2026-01-04 04:04:03', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -177,7 +339,7 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('e8NqkW4fRHZeF7RQn5GUniGZU2s3JOVuXYyc95W8', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoicUN1TkFXY3hTWHlZc3ZPSGlwWFZzNW9pUW9Vc21Db1dDU2VRS2EyWiI7czo5OiJfcHJldmlvdXMiO2E6Mjp7czozOiJ1cmwiO3M6MzM6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9hZG1pbi91c2VycyI7czo1OiJyb3V0ZSI7czoxMToiYWRtaW4udXNlcnMiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjUyOiJsb2dpbl9hZG1pbl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjE7fQ==', 1762440949);
+('reZAowo2N2FeDibD0iZWOBfYqJLT4ntstciy5A8u', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoib1BIdTg4d0ZuZXU1S1BvQUpRelE5YTJRTE0zU0Y1NG1jcTY0ajF2ZiI7czo5OiJfcHJldmlvdXMiO2E6Mjp7czozOiJ1cmwiO3M6NDQ6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9hZG1pbi9wcm9kdWN0LWNhdGVnb3J5IjtzOjU6InJvdXRlIjtzOjI3OiJhZG1pbi5wcm9kdWN0Y2F0ZWdvcnkuaW5kZXgiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjUyOiJsb2dpbl9hZG1pbl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjE7fQ==', 1767525548);
 
 -- --------------------------------------------------------
 
@@ -272,16 +434,57 @@ ALTER TABLE `job_batches`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `main_category`
+--
+ALTER TABLE `main_category`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `migrations`
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `pages`
+--
+ALTER TABLE `pages`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `slug` (`slug`);
+
+--
+-- Indexes for table `page_category`
+--
+ALTER TABLE `page_category`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `slug` (`slug`);
+
+--
 -- Indexes for table `password_reset_tokens`
 --
 ALTER TABLE `password_reset_tokens`
   ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `slug` (`slug`);
+
+--
+-- Indexes for table `product_category`
+--
+ALTER TABLE `product_category`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `slug` (`slug`);
+
+--
+-- Indexes for table `product_sub_category`
+--
+ALTER TABLE `product_sub_category`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `slug` (`slug`);
 
 --
 -- Indexes for table `sessions`
@@ -328,10 +531,46 @@ ALTER TABLE `jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `main_category`
+--
+ALTER TABLE `main_category`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `pages`
+--
+ALTER TABLE `pages`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `page_category`
+--
+ALTER TABLE `page_category`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `product_category`
+--
+ALTER TABLE `product_category`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `product_sub_category`
+--
+ALTER TABLE `product_sub_category`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
